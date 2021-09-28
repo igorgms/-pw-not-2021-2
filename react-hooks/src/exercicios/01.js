66cefaad2e41c076b74695b4dcee5fd49eadd0f8
igorgms <igorgmsf@gmail.com>
2021-09-28 23:12:52 +0000

    ajustes

--- react-hooks/src/exercicios/01.js
+++ react-hooks/src/exercicios/01.js
@@ -1,19 +1,21 @@
 import * as React from 'react'
 
 export default function Exercicio01() {
-    const [name, setName] = React.useState('')
+  // 💣 delete this variable declaration and replace it with a React.useState call
+  const [name, setName] = React.useState('')
 
-    function handleChange(event) {
-        setName(event.target.value)
-    }
+  function handleChange(event) {
+    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
+  }
 
-    return (
-        <div>  
-            <form>
-                <label htmlFor="name">Name: </label>   
-                <input onChange={handleChange} id="name" />
-            </form>
-            {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-        </div>
-    )
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
 }
--- react-hooks/src/exercicios/Teste1.js
+++ react-hooks/src/exercicios/Teste1.js
@@ -1,8 +1,8 @@
 export default function Teste1() {
-    return (
-        <>
-            <h1>Primeiro componente React</h1>
-            <p>Este é o primeiro componente React.</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Primeiro componente React</h1>
+      <p>Este é o primeiro componente React.</p>
+    </>
+  )
 }
